af1597713dfef439965db8d46433f081a4c5af02
Michal <m.krepiniewicz@gmail.com>
2025-03-28 20:45:16 +0100

    Challenge part 1

---
+++ challenge-1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Car = function () {
+  this.make = 0;
+  this.speed = 0;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.speed} km/h`);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.speed} km/h`);
+};
+
+const car1 = new Car();
+const car2 = new Car();
+
+car1.accelerate();
+car2.break();
+
+function Car2() {
+  this.make = 0;
+  this.speed = 0;
+}
+
+Car2.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.speed} km/h`);
+  return this;
+};
+
+const car3 = new Car2();
+car3.accelerate();
+car3.accelerate();
+car3.accelerate().accelerate();
