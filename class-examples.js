bf3e5bc1bcde864805bd2d795fc357915cf47d3a
Michal <m.krepiniewicz@gmail.com>
2025-04-08 18:55:39 +0200

    Prolog to Encapsulation of data!

---
+++ class-examples.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved.`);
+    }
+  }
+}
